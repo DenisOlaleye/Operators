5747017d962dac374f025d8a2d3afb089308dcc0
DenisOlaleye <denis.olaleye@gmail.com>
2022-08-23 10:10:59 +0300

    rewriting

---
+++ EqualityAndRelational.java
@@ -0,0 +1,18 @@
+
+public class EqualityAndRelational 
+{
+	public static void main(String[] args) 
+	{
+		
+	
+	int count = 1;
+	
+	Object object1 = new Object();
+	Object object2 = new Object();
+	Object object3 = null;
+	System.out.println(object1==object2);
+	System.out.println(object3==null);
+	
+	}
+	
+}
